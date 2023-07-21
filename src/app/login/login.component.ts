6b92dd4acf69bd6596a35d32c01e5e2331837f37
Stefan Kupresak <stefan_vg@hotmail.com>
2023-07-21 23:51:20 +0200

    finish google user auth

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  constructor(private authService: AuthService) {}
 
+  loginWithGoogle(event: MouseEvent): void {
+    event.preventDefault();
+    this.authService.loginWithGoogle();
+  }
 }
---
+++ src/app/shared/auth.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, OnDestroy, inject } from '@angular/core';
+import { User } from './user';
+import {
+  Auth,
+  GoogleAuthProvider,
+  authState,
+  signInWithPopup,
+} from '@angular/fire/auth';
+import { OrNull } from './types';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService implements OnDestroy {
+  auth = inject(Auth);
+  router = inject(Router);
+  authState = authState(this.auth);
+  user: OrNull<User> | null = null;
+  subscription: Subscription;
+
+  constructor() {
+    this.subscription = this.authState.subscribe((user) => {
+      console.log('user', user);
+      this.user = user;
+    });
+  }
+
+  loginWithGoogle(): void {
+    signInWithPopup(this.auth, new GoogleAuthProvider()).then((result) => {
+      if (result.user) {
+        this.router.navigate(['/']);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+}
